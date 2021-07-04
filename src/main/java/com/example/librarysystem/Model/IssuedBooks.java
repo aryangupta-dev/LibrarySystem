5c35b1e25b5f081d19a019c2b5c8d9768a233505
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-07-04 11:29:07 +0530

    Created book class

---
+++ src/main/java/com/example/librarysystem/Model/IssuedBooks.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan Gupta (ag2602)
+ *   Date: 7/4/2021
+ *   Time: 11:13 AM
+ *   File: IssuedBooks.java
+ */
+
+package com.example.librarysystem.Model;
+
+public class IssuedBooks {
+}
